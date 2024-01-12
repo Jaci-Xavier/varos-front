3e69d5ec2efaae56dfde4be2d330a1dd37fc9ce4
Jaci-Xavier <devxvr@gmail.com>
2024-01-12 20:02:49 -0300

    adiciona react slick e slick carousel

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,8 +3,8 @@ import arrow from '../../assets/arrow.png';
 function Hero() {
   return (
     <div className='flex flex-col w-[35.25em] h-[22.8em] gap-[2em] items-start text-start mt-24 ml-24'>
-      <h1>Investir de forma mais inteligente passa por aqui.</h1>
-      <h2>Fazemos de tudo para que você possa conquistar seus sonhos da melhor forma possível.</h2>
+      <h1 className='text-6xl font-extrabold leading-[70px] tracking[0em] text-left w-[564px] h-[210px]'>Investir de forma mais inteligente passa por aqui.</h1>
+      <h2 className='w-[469px] h-[44px] text-[18px] font-semibold leading-5 tracking-[0em] text-left'>Fazemos de tudo para que você possa conquistar seus sonhos da melhor forma possível.</h2>
       <button 
         className='flex flex-row bg-[#19C819] rounded-[40px] text-[rgba(0, 247, 0, 1)] w-[11em] h-[3em] text-[1em] items-center justify-center'
         type="button"
--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,4 +1,12 @@
 import { NavItem } from '../types';
+import g1 from '../assets/Group.png';
+import g2 from '../assets/Group2.png';
+import g3 from '../assets/Group3.png';
+import g4 from '../assets/Group4.png';
+import g5 from '../assets/Group5.png';
+import g6 from '../assets/Group6.png';
+
+const cards = [g1, g2, g3, g4, g5, g6];
 
 const nav: Array<NavItem> = [
   { name: 'Produtos', path: '/produtos' },
@@ -7,4 +15,4 @@ const nav: Array<NavItem> = [
   { name: 'Quem Somos', path: '/quem-somos' },
 ];
 
-export { nav };
+export { nav, cards };
