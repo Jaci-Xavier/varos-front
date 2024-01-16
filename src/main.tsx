7e22242b935c6d20ce1ca710d70e5ecb16c97be2
Jaci-Xavier <devxvr@gmail.com>
2024-01-16 18:34:38 -0300

    limpando codigo inutil

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
-// import { InputContextProvider } from './Context/InputContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <React.StrictMode>
-      {/* <InputContextProvider> */}
         <App />
-      {/* </InputContextProvider> */}
     </React.StrictMode>
   </BrowserRouter>
 )
