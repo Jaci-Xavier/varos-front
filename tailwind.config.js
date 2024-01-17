bdf5f2b425293c6d1dae6b3d23c10b785347a720
Jaci-Xavier <devxvr@gmail.com>
2024-01-17 08:48:31 -0300

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      mobile: '640px',
+      tablet: '768px',
+      desktop: '1024px',
+    },
   },
   plugins: [],
-}
-
+};
